c174e2d5c59ff59aa202518cedd10b055562a2e6
Yunip Shrestha <yunipshrestha@gmail.com>
2021-12-26 01:17:33 +0545

    Javascript/image-carousel indicators changing completed

--- Javascript/ImageCarousel/js/Carousel.js
+++ Javascript/ImageCarousel/js/Carousel.js
@@ -6,51 +6,72 @@
 const carousel = document.getElementById('image_carousel');
 const imagesWrapper = document.getElementById('image_wrapper');
 const images = imagesWrapper.children;
-
-//create Navigation container
-const navigation = document.createElement("div");
-navigation.id = "navigation"
-carousel.append(navigation);
-
-//create dot container
-const dotContainer = document.createElement("div");
-navigation.id = "carousel-dots-container"
-navigation.append(dotContainer);
-
-// const dotContainer = document.getElementById('carousel-dots-container')
-
 const imageWidth = images[0].width;
 const imageCount = images.length;
 // carousel.style.height = `${images[0].height}px`;  // comment this
 imagesWrapper.style.height = `${images[0].height}px`;
 
-//Carousel Dots
+let currentIndex = 0;
+let carouselPosition = 0;
+const speed = 10;
+
+//create Navigation container
+const navigation = document.createElement("div");
+navigation.id = "carousel-navigation"
+carousel.append(navigation);
+
+//create dot container
+const dotContainer = document.createElement("div");
+dotContainer.id = "carousel-dots-container"
+navigation.append(dotContainer);
+
+// add Carousel Dots dynamically
 for (let i = 0; i<imageCount; i++){
     let dot = document.createElement('div');
     dot.setAttribute('class','carousel-dot');
     dotContainer.append(dot);
 }
+dotContainer.children[currentIndex].classList.add('active');
 
 // Aligning images in a horizontal line
-let carouselPosition = 0;
 for (let i = 0; i < images.length; i++){
     images[i].style.left = `${imageWidth * i}px`;
 }
 
-//create control buttons
+//create controls container
+const controls = document.createElement("div");
+controls.id = "carousel-controls"
+navigation.append(controls);
 
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
+//create prev control buttons
+const prev = document.createElement("div");
+prev.id = "prev";
+const prevBtn = document.createElement('img');
+prevBtn.setAttribute('src','https://img.icons8.com/external-those-icons-lineal-those-icons/48/000000/external-left-arrows-those-icons-lineal-those-icons-4.png');
+prev.append(prevBtn);
+controls.append(prev);
 
-let currentIndex = 0;
-const dx = 10;
+//create next control buttons
+const next = document.createElement("div");
+next.id = "next";
+const nextBtn = document.createElement('img');
+nextBtn.setAttribute('src','https://img.icons8.com/external-those-icons-lineal-those-icons/48/000000/external-right-arrows-those-icons-lineal-those-icons-3.png');
+next.append(nextBtn);
+controls.append(next);
+
+
+
+
+
 
 const handleNext = () => {
+    dotContainer.children[currentIndex].classList.toggle('active');
     (currentIndex === imageCount-1) ? currentIndex = 0 : currentIndex++;
+    dotContainer.children[currentIndex].classList.toggle('active');
     // imagesWrapper.style.left = `-${imageWidth * currentIndex}px`;
     if (currentIndex){
         const transition = setInterval(() => {
-            carouselPosition = carouselPosition + dx;
+            carouselPosition = carouselPosition + speed;
             // console.log((carouselPosition % imageWidth))
             if (carouselPosition > imageWidth * currentIndex){
                 carouselPosition = imageWidth * currentIndex;
@@ -63,7 +84,7 @@ const handleNext = () => {
     else{
         // imagesWrapper.style.left = "0px";
         const transition = setInterval(() => {
-            carouselPosition = carouselPosition - dx;
+            carouselPosition = carouselPosition - speed;
             if (carouselPosition < 0) carouselPosition = 0;
             imagesWrapper.style.left = `-${carouselPosition}px`;
             if (carouselPosition == 0) clearInterval(transition);
@@ -72,9 +93,10 @@ const handleNext = () => {
 }
 const handlePrev = () => {
     // imagesWrapper.style.left = `-${imageWidth * currentIndex}px`;
+    dotContainer.children[currentIndex].classList.toggle('active');
     if (currentIndex){
         const transition = setInterval(() => {
-            carouselPosition = carouselPosition - dx;
+            carouselPosition = carouselPosition - speed;
             // console.log((carouselPosition % imageWidth))
             if (carouselPosition < imageWidth * currentIndex){
                 carouselPosition = imageWidth * currentIndex;
@@ -87,7 +109,7 @@ const handlePrev = () => {
     else{
         imagesWrapper.style.left = "0px";
         const transition = setInterval(() => {
-                carouselPosition = carouselPosition + dx;
+                carouselPosition = carouselPosition + speed;
                 if (carouselPosition > imageWidth * currentIndex) {
                     carouselPosition = imageWidth * currentIndex;
                     clearInterval(transition);
@@ -97,11 +119,13 @@ const handlePrev = () => {
             }, 0);
     }
     (currentIndex === 0) ? currentIndex = imageCount-1 : currentIndex--;
+    dotContainer.children[currentIndex].classList.toggle('active');
 }
 // const handlePrev = () => {
 //     (currentIndex == 0) ? currentIndex = 4 : currentIndex--;
 //     imagesWrapper.style.left = `-${imageWidth * currentIndex}px`;
 // }
 
-nextBtn.addEventListener('click', handleNext);
-prevBtn.addEventListener('click', handlePrev);
+
+prev.addEventListener('click', handlePrev);
+next.addEventListener('click', handleNext);
