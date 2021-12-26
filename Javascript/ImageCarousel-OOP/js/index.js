2d17307b04edbd1536ba6852d0afe987a3f6798d
Yunip Shrestha <yunipshrestha@gmail.com>
2021-12-27 01:32:39 +0545

    Javascript/Image Carousel OOP: Completed

--- Javascript/ImageCarousel-OOP/js/index.js
+++ Javascript/ImageCarousel-OOP/js/index.js
@@ -5,4 +5,4 @@
 //holdTime => slide changes automatically after given time
 
 var Carousel1 = new Carousel('image_carousel',600,3000,5000);
-var Carousel2 = new Carousel('image_carousel1',900,1000,2000);
+var Carousel2 = new Carousel('image_carousel1',700,1000,2000);
