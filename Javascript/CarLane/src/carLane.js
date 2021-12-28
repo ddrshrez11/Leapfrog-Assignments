c718905315ba58ee69df7403f06cc5b113f6eaf9
Yunip Shrestha <yunipshrestha@gmail.com>
2021-12-29 04:48:56 +0545

    Javascript/CarLane : start bullet implementation

--- Javascript/CarLane/src/carLane.js
+++ Javascript/CarLane/src/carLane.js
@@ -3,31 +3,52 @@
 const actionArea = {
   viewport: document.getElementById("viewport"),
   canvas: document.createElement("canvas"),
+
+  
   
   startAction: function () {
     this.canvas.width = "900";
     this.canvas.height = "500";
     this.canvas.style.border = "2px solid black";
     this.context = this.canvas.getContext("2d");
+    
+    // this.initScores();
+
     document.body.addEventListener("keydown", this.handleKeyPress);
     this.viewport.appendChild(this.canvas);
     this.viewport.style.width = '100%';
     this.viewport.style.textAlign = 'center';
     this.interval = setInterval(updateActionArea, FPS);
   },
+  // initScores: function(){
+  //   this.score.innerHTML='0';
+  //   this.scoreDiv.innerHTML = 'SCORE: ';
+  //   this.scoreDiv.appendChild(this.score);
+  //   this.viewport.appendChild(this.scoreDiv);
+  //   this.scoreDiv.style.left="20%";
+  //   this.scoreDiv.style.display="inline-block";
+  //   this.highscoreDiv.style.display="inline-block";
+
+  //   this.highscore.innerHTML='0';
+  //   this.highscoreDiv.innerHTML = 'HIGHSCORE: ';
+  //   this.highscoreDiv.appendChild(this.highscore);
+  //   this.highscoreDiv.style.left="20%";
+  //   this.viewport.appendChild(this.highscoreDiv);
+
+  // },
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
-  winGame: function () {
-    this.context.textAlign = "center";
-    this.context.fillStyle = "Blue";
-    this.context.font = "100px Ubuntu";
-    this.context.fillText(
-      "YOU WIN",
-      this.canvas.width / 2,
-      this.canvas.height / 2
-    );
-  },
+  // winGame: function () {
+  //   this.context.textAlign = "center";
+  //   this.context.fillStyle = "Blue";
+  //   this.context.font = "100px Ubuntu";
+  //   this.context.fillText(
+  //     "YOU WIN",
+  //     this.canvas.width / 2,
+  //     this.canvas.height / 2
+  //   );
+  // },
   handleKeyPress: function (e) {
     console.log(e);
     if (e.keyCode == '37'){
@@ -39,6 +60,53 @@ const actionArea = {
       car.moveRight();
     }
 
+  },
+  gameOver: function () {
+    clearInterval(this.interval);
+    if (score>highscore){
+      localStorage.setItem("highscore",score);
+    }
+    this.viewport.position = "relative";
+    const gameoverDiv = document.createElement("div");
+    gameoverDiv.id ="gameOverDiv";
+    this.viewport.append(gameoverDiv);
+    gameoverDiv.style.width = "900px";
+    gameoverDiv.style.height = "400px";
+    gameoverDiv.style.position = "absolute";
+    gameoverDiv.style.top = "5px";
+    gameoverDiv.style.left = "50%";
+    // gameoverDiv.style.right = "0";
+    gameoverDiv.style.transform = "translateX(-50%)";
+    gameoverDiv.style.backgroundColor = "black";
+    gameoverDiv.style.textAlign = "center";
+    gameoverDiv.style.color = "white";
+    gameoverDiv.style.fontSize = "100px";
+    gameoverDiv.style.fontFamily = "Ubuntu";
+    gameoverDiv.style.paddingTop = "10%";
+    gameoverDiv.innerHTML = "Game Over";
+
+
+
+    
+    const reloadBtn = document.createElement("div");
+    reloadBtn.style.position = "absolute";
+    reloadBtn.style.height = "50px";
+    reloadBtn.style.width = "150px";
+    reloadBtn.style.padding = "10px";
+    reloadBtn.style.borderRadius = "20%";
+
+    reloadBtn.style.fontSize = "40px";
+    reloadBtn.style.backgroundColor = "red";
+    reloadBtn.style.bottom = "30px";
+    reloadBtn.style.marginLeft = "50%";
+    reloadBtn.style.transform = "translateX(-50%)"
+    // reLoad.style.marginBottom = "30px";
+    reloadBtn.innerText = "Reload";
+    gameoverDiv.append(reloadBtn);
+    reloadBtn.onclick = function () {
+      window.location.reload();
+      startGame();
+    };
   }
 };
 
@@ -53,7 +121,7 @@ function Background(speed){
     ctx = actionArea.context;
     ctx.globalCompositeOperation='destination-over';
     ctx.drawImage(this.img, 0, this.y, actionArea.canvas.width, actionArea.canvas.height);
-    ctx.drawImage(this.img, 0, this.y - this.height,900,500);
+    ctx.drawImage(this.img, 0, this.y - this.height,actionArea.canvas.width,actionArea.canvas.height);
     this.y -= this.step;
     if(this.y > this.max){
       this.y =0;
@@ -69,14 +137,12 @@ function Obstacle(w, h, y){
   this.h = h;
   this.y = y;
   this.initialY = y;
-  this.speed = speed;
   this.dy = 1;
   this.index = getRandomIndex();
   this.x = laneMap[this.index];
 
   this.update = function () {
     ctx = actionArea.context;
-    ctx.globalCompositeOperation='destination-over';
     this.img = document.createElement("img");
     this.img.src = "images/obstacle.png";
     ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
@@ -86,7 +152,7 @@ function Obstacle(w, h, y){
     }
   }
   this.moveObstacle = function () {
-    this.y += this.dy * this.speed;
+    this.y += this.dy * speed;
   }
   this.reset = function() {
     score++;
@@ -102,28 +168,35 @@ function Car(w, h) {
   this.h = h;
   this.y = 400;
   this.index = 1;
+  this.bulletCount = 0;
   this.x = laneMap[this.index];
   // this.color = color;
   this.speed = speed;
   this.update = function () {
     ctx = actionArea.context;
     this.img = document.createElement("img");
-    this.img.src = "images/Car.png";
-    this.update = () => {
-      ctx = actionArea.context;
+    this.img.src = "images/car.png";
+    
       ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
-    };
-    this.moveLeft = function() {
-      if (this.index > 0){
-        this.index--;
-        this.x = laneMap[this.index];
-      }
-    }
-    this.moveRight = function() {
-      if (this.index < laneMap.length){
-        this.index++;
-        this.x = laneMap[this.index];
-      }
+  };
+  this.moveLeft = function() {
+    if (this.index > 0){
+      this.index--;
+      this.x = laneMap[this.index];
     }
   }
+  this.moveRight = function() {
+    if (this.index < laneMap.length){
+      this.index++;
+      this.x = laneMap[this.index];
+    }
+  }
+  this.fireBullet = function(){
+    
+  }
+}
+
+function Bullet(w,h){
+  this.w = w;
+  this.h = h;
 }
--- Javascript/CarLane/src/index.js
+++ Javascript/CarLane/src/index.js
@@ -8,13 +8,55 @@ const laneMap = [200,440,650];
 //     1:440, //laneMiddle
 //     2:650, //laneRight
 let score = 0;
+let highscore = 0;
 let index = 1;
 const carHeight = 70;
 const carWidth = 40;
 
-
-
 function start(){
+    actionArea.viewport.position = "relative";
+    const startMenu = document.createElement("div");
+    startMenu.id ="startMenu";
+    actionArea.viewport.append(startMenu);
+    startMenu.style.width = "900px";
+    startMenu.style.height = "400px";
+    startMenu.style.position = "absolute";
+    startMenu.style.top = "5px";
+    startMenu.style.left = "50%";
+    // startMenu.style.right = "0";
+    startMenu.style.transform = "translateX(-50%)";
+    startMenu.style.backgroundColor = "blue";
+    startMenu.style.textAlign = "center";
+    startMenu.style.color = "white";
+    startMenu.style.fontSize = "100px";
+    startMenu.style.fontFamily = "Ubuntu";
+    startMenu.style.paddingTop = "10%";
+    startMenu.innerHTML = "Car Lane Game";
+    // actionArea.startAction();
+
+    const startBtn = document.createElement("div");
+    startBtn.style.position = "absolute";
+    startBtn.style.height = "50px";
+    startBtn.style.width = "150px";
+    startBtn.style.padding = "10px";
+    startBtn.style.borderRadius = "20%";
+
+    startBtn.style.fontSize = "40px";
+    startBtn.style.backgroundColor = "red";
+    startBtn.style.bottom = "30px";
+    startBtn.style.marginLeft = "50%";
+    startBtn.style.transform = "translateX(-50%)"
+    // reLoad.style.marginBottom = "30px";
+    startBtn.innerText = "Play";
+    startMenu.append(startBtn);
+    startBtn.onclick = function () {
+      startGame(startMenu);
+    };
+}
+
+
+function startGame(startMenu){
+    startMenu.style.display = 'none';
     actionArea.startAction();
     car = new Car(carWidth,carHeight);
     laneBackground = new Background(speed);
@@ -22,17 +64,23 @@ function start(){
     obstacle2 = new Obstacle(carWidth,carHeight,-250);
     obstacles.push(obstacle1);
     obstacles.push(obstacle2);
+    if (localStorage.getItem("highscore") !== null){
+        highscore = (localStorage.getItem("highscore"));
+    }
+    let speedUpdate =setInterval(() => {
+        speed++;
+    }, 5000);
 
 }
 
 function collisionDetect(obstacle) {
     if (
-      obstacle.x + 60 > car.x &&
-      obstacle.y < car.y + 70 &&
-      obstacle.x < car.x + 40 &&
-      95 + obstacle.y > car.y
+      obstacle.x + carWidth > car.x &&
+      obstacle.y < car.y + carHeight &&
+      obstacle.x < car.x + carWidth &&
+      obstacle.y + carHeight  > car.y
     ) {
-    //   gameOver();
+      actionArea.gameOver();
     console.log('Game Over')
     }
   }
@@ -47,10 +95,15 @@ function updateActionArea() {
         obstacle.update()
     });
     laneBackground.print();
+    
   }
 
 function displayScore(){
-    actionArea.context.font = "50px Ubuntu";
+    actionArea.context.font = "30px Ubuntu";
     actionArea.context.fillStyle = "white";
     actionArea.context.fillText(`Score: ${score}`,150, 50 );
+    actionArea.context.font = "30px Ubuntu";
+    actionArea.context.fillStyle = "white";
+    actionArea.context.fillText(`Highscore: ${highscore}`,350, 50 );
+
 }
