31a1956104fb0cb676ee5e940e17616434e81d43
Yunip Shrestha <yunipshrestha@gmail.com>
2022-01-07 00:21:30 +0545

    JS/FishAquarium: Cleaning junk feature added

--- Javascript/FishAquarium/js/junk.js
+++ Javascript/FishAquarium/js/junk.js
@@ -4,6 +4,7 @@ export default class Junk {
      * @param {Game} game Game object
      */
     constructor(game) {
+        this.game = game;
         this.gameWidth = game.gameWidth;
         this.gameHeight = game.gameHeight;
         this.r = getRandomFromRange(20, 30);
@@ -11,6 +12,7 @@ export default class Junk {
             x: getRandomFromRange(this.r, this.gameWidth - this.r),
             y: getRandomFromRange(this.r, this.gameHeight - this.r),
         };
+        this.cleaned = false;
     }
 
     /**
@@ -18,7 +20,7 @@ export default class Junk {
      * @param {context} ctx Context of canvas
      */
     draw = (ctx) => {
-        console.log("draw");
+        // console.log("draw");
         ctx.beginPath();
         ctx.fillStyle = "red";
         ctx.arc(this.position.x, this.position.y, this.r, 0, 2 * Math.PI);
@@ -31,6 +33,25 @@ export default class Junk {
      * @param {number} deltaTime change in time from previous frame
      */
     update = (deltaTime) => {
+        if (
+            this.game.mouse.click &&
+            this.game.gameMode === this.game.gameModes.CLEAN
+        ) {
+            const dx = this.position.x - this.game.mouse.x;
+            const dy = this.position.y - this.game.mouse.y;
+            if (Math.abs(getDistance(dx, dy)) < this.r) {
+                // console.log("clean");
+                // if (
+                //     this.game.mouse.x >= this.position.x - this.r &&
+                //     this.game.mouse.x <= this.position.x + this.r &&
+                //     this.game.mouse.y >= this.position.y - this.r &&
+                //     this.game.mouse.y > this.position.y + this.r
+                // ) {
+                this.cleaned = true;
+                this.game.updateJunks();
+                this.game.mouse.click = false;
+            }
+        }
         // this.wallCollisionDetect();
         // this.position.y += (this.direction.y * this.speed) / deltaTime;
         // console.log(this.position.y, this.direction.y, deltaTime);
